7a71642dae89716a9e6a7423edc4d246e984953c
elicorrales <e_corrales2003@yahoo.com>
2019-12-27 18:34:35 -0500

    .

--- Python/Motor.Drive.Test/loopTestMotorDrive.py
+++ Python/Motor.Drive.Test/loopTestMotorDrive.py
@@ -10,16 +10,16 @@ import random
 import time
 
 parser = argparse.ArgumentParser(prog=sys.argv[0], description='detect object with webcam & opencv', allow_abbrev=False)
-#parser.add_argument('--width',type=int, dest='width', required=True)
-#parser.add_argument('--height',type=int, dest='height', required=True)
-#parser.add_argument('--FPS',type=int, dest='FPS', required=True)
-#parser.add_argument('--limit-buffer',dest='limitBuffer', action='store_true')
-#args = parser.parse_args()
+parser.add_argument('--http-timeout',type=float, dest='httpTimeout', required=True)
+parser.add_argument('--speed',type=int, dest='speed', required=True)
+parser.add_argument('--loopDelay',type=float, dest='loopDelay', required=True)
+parser.add_argument('--single-direction',dest='oneDirection', action='store_true')
+args = parser.parse_args()
 
-#width=args.width
-#height=args.height
-#FPS=args.FPS
-#limitBuffer=args.limitBuffer
+httpTimeout=args.httpTimeout
+speed=args.speed
+loopDelay=args.loopDelay
+oneDirection=args.oneDirection
 
 robotDriveUrl = 'http://10.0.0.58:8084'
 robotIsReadyToDrive = False
@@ -47,23 +47,26 @@ def say(phrase):
     tts.runAndWait()
 
 ##################################################################
-def sendRobotUrl(command, mytimeout = 0.1):
+def sendRobotUrl(command):
 
     try:
-        response = requests.get(robotDriveUrl + command, timeout=mytimeout)
+        response = requests.get(robotDriveUrl + command, timeout=httpTimeout)
         return response.text
     except (socket.timeout, urllib3.exceptions.ReadTimeoutError, requests.exceptions.ReadTimeout):
         #track = traceback.format_exc()
         #print(track)
+        print('The request timed out.')
         say('The request timed out.')
         return ''
     except (requests.exceptions.ConnectionError, ConnectionRefusedError):
+        print('Connection Refused. Did You start the node J S server?')
         say('Connection Refused. Did You start the node J S server?')
         cleanUp()
     except:
         track = traceback.format_exc()
         print(track)
         say('Other Communication Error')
+        cleanUp()
 
 ##################################################################
 def initRobotDrive():
@@ -84,7 +87,7 @@ def sendRobotDriveCommand(direction):
     if not robotIsReadyToDrive:
         robotIsReadyToDrive = initRobotDrive()
     if robotIsReadyToDrive:
-        respText = sendRobotUrl('/arduino/api/' + direction + '/100')
+        respText = sendRobotUrl('/arduino/api/' + direction + '/' + str(speed))
         if not 'Cmd Sent To Arduino' in respText:
             print(respText)
             say(respText)
@@ -122,6 +125,12 @@ robotIsReadyToDrive = initRobotDrive()
 if robotIsReadyToDrive:
 
     while(True):
+
+        if oneDirection:
+            sendRobotDriveCommand('forward')
+            time.sleep(0.08)
+            continue
+
         rand = random.randint(1,100)
         if rand >= 1 and rand < 25:
             #say('right')
@@ -138,7 +147,8 @@ if robotIsReadyToDrive:
         else:
             say('random was ' + rand)
 
-        time.sleep(0.08)
+        if loopDelay > 0.01:
+            time.sleep(loopDelay)
 
 else:
     print('Failed to initialize robot drive')
--- Python/OpenCV/FollowMe/loopFollowMe.py
+++ Python/OpenCV/FollowMe/loopFollowMe.py
@@ -15,12 +15,22 @@ parser = argparse.ArgumentParser(prog=sys.argv[0], description='detect object wi
 parser.add_argument('--width',type=int, dest='width', required=True)
 parser.add_argument('--height',type=int, dest='height', required=True)
 parser.add_argument('--FPS',type=int, dest='FPS', required=True)
+parser.add_argument('--http-timeout',type=float, dest='httpTimeout', required=True)
+parser.add_argument('--speed',type=int, dest='speed', required=True)
+parser.add_argument('--loopDelay',type=float, dest='loopDelay', required=True)
+parser.add_argument('--min-dist',type=int, dest='minDist', required=True)
+parser.add_argument('--max-dist',type=int, dest='maxDist', required=True)
 parser.add_argument('--limit-buffer',dest='limitBuffer', action='store_true')
 args = parser.parse_args()
 
 width=args.width
 height=args.height
 FPS=args.FPS
+httpTimeout=args.httpTimeout
+speed=args.speed
+loopDelay=args.loopDelay
+minDist=args.minDist
+maxDist=args.maxDist
 limitBuffer=args.limitBuffer
 
 robotDriveUrl = 'http://10.0.0.58:8084'
@@ -32,6 +42,7 @@ cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 cap.set(cv2.CAP_PROP_FPS, FPS)
+
 if limitBuffer:
     cap.set(cv2.CAP_PROP_BUFFERSIZE,1)
 
@@ -41,7 +52,18 @@ xml_path = '/home/devchu/.virtualenvs/cv/lib/python3.7/site-packages/cv2/data/'
 face_cascade = cv2.CascadeClassifier(xml_path + 'haarcascade_frontalface_default.xml')
 
 
+##################################################################
+# these program flow control, mainly just to make sure we do not
+# repeat(say) something uneccesarily.
 noFaceDetected = False
+faceDetected   = False
+faceCentered   = False
+tooManyFaces   = False
+faceMovedLeft  = False
+faceMovedRight = False
+faceTooClose   = False
+faceTooFar     = False
+
 lastTimeMoved = time.time()
 
 tts = pyttsx3.init()
@@ -66,18 +88,18 @@ def say(phrase):
     tts.runAndWait()
 
 ##################################################################
-def sendRobotUrl(command, mytimeout=0.1):
+def sendRobotUrl(command):
 
     global robotDriveServerConnectionRefusedNumberOfTimes
 
     try:
-        response = requests.get(robotDriveUrl + command, timeout=mytimeout)
+        response = requests.get(robotDriveUrl + command, timeout=httpTimeout)
         robotDriveServerConnectionRefusedNumberOfTimes = 0;
         return response.text
     except (socket.timeout, urllib3.exceptions.ReadTimeoutError, requests.exceptions.ReadTimeout):
         return 'Timeout'
     except (requests.exceptions.ConnectionError, ConnectionRefusedError):
-        time.sleep(1)
+        time.sleep(0.20)
         robotDriveServerConnectionRefusedNumberOfTimes += 1;
         if robotDriveServerConnectionRefusedNumberOfTimes > 3:
             cleanUp()
@@ -102,12 +124,12 @@ def initRobotDrive():
     return False
 
 ##################################################################
-def sendRobotDriveCommand(direction):
+def sendRobotDriveCommand(direction, mySpeed=speed):
     global robotIsReadyToDrive
     if not robotIsReadyToDrive:
         robotIsReadyToDrive = initRobotDrive()
     if robotIsReadyToDrive:
-        respText = sendRobotUrl('/arduino/api/' + direction + '/100')
+        respText = sendRobotUrl('/arduino/api/' + direction + '/' + str(mySpeed))
         if not 'Cmd Sent To Arduino' in respText:
             print(respText)
             say(respText)
@@ -140,6 +162,8 @@ if __name__ == '__main__':
 
 
 robotIsReadyToDrive = initRobotDrive()
+robotIsReadyToDrive = initRobotDrive()
+robotIsReadyToDrive = initRobotDrive()
 
 # only attempt to read if it is opened
 if cap.isOpened:
@@ -147,14 +171,39 @@ if cap.isOpened:
         ret, frame = cap.read()
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-        if len(faces) < 1 and not noFaceDetected:
-            say('No one')
-            noFaceDetected = True
-            lastTimeMoved = time.time()
+
+        if len(faces) < 1:
+            if not noFaceDetected:
+                say('No one')
+                noFaceDetected = True
+            faceDetected = False
+            tooManyFaces = False
+            faceCentered = False
+            faceMovedLeft = False
+            faceMovedRight = False
+            faceTooClose   = False
+            faceTooFar     = False
             continue
 
+        elif len(faces) > 2:
+            if not tooManyFaces:
+                say('too many faces')
+                tooManyFaces = True
+            noFaceDetected = False
+            faceDetected = False
+            continue
 
+        elif len(faces) > 0:
+            noFaceDetected = False
+            if not faceDetected:
+                say('I see you')
+                faceDetected = True
+                #continue
+
+        tooManyFaces = False
+        faceIdx = 0
         for (x, y, w, h) in faces:
 
             noFaceDetected = False
@@ -166,15 +215,48 @@ if cap.isOpened:
 
             deltaLastTimeMoved = time.time() - lastTimeMoved
 
-            if deltaLeftRight > 20 and deltaLastTimeMoved > 0.2:
-                if leftEdge > rightEdge:
-                    sendRobotDriveCommand('right')
+            ####################################################
+            # only if already centered, do we move toward or away
+            if deltaLeftRight <= 40:
+
+                faceMovedLeft = False
+                faceMovedRight = False
+
+                if not faceCentered:
+                    say('center')
+                    faceCentered = True
+
+                if w > minDist and deltaLastTimeMoved > loopDelay:
+                    if not faceTooClose:
+                        say('close')
+                        faceTooClose = True
+                    sendRobotDriveCommand('backward', 43)
                     lastTimeMoved = time.time()
-                else:
-                    sendRobotDriveCommand('left')
+                elif w < maxDist and deltaLastTimeMoved > loopDelay:
+                    if not faceTooFar:
+                        say('far')
+                        faceTooFar = True
+                    sendRobotDriveCommand('forward', 43)
                     lastTimeMoved = time.time()
 
+ 
+            else:
+                faceCentered = False
+                if deltaLastTimeMoved > loopDelay:
 
+                    if leftEdge > rightEdge:
+                        if not faceMovedRight:
+                            say('right')
+                            faceMovedRight = True
+                        sendRobotDriveCommand('right')
+                    else:
+                        if not faceMovedLeft:
+                            say('left')
+                            faceMovedLeft = True
+                        sendRobotDriveCommand('left')
+
+                    lastTimeMoved = time.time()
+ 
 else:
     print("Failed to open capture device")
 
