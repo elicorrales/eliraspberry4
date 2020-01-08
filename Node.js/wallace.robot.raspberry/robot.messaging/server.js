3dfbbd4934d9e4ff8d51d30278867ea4a3041ebb
elicorrales <e_corrales2003@yahoo.com>
2020-01-08 15:04:28 +0000

    .

--- Node.js/wallace.robot.raspberry/robot.messaging/server.js
+++ Node.js/wallace.robot.raspberry/robot.messaging/server.js
@@ -15,26 +15,61 @@ app.use((request, response, next) => {
 const thisServerPort = 8085;
 
 
+let latestCommandToRobotDrive = '';
+let latestStatusFromRobotDrive = '';
 
+const respondeWithCollectedDataHandler = (request, response) => {
+        response.status(200).send('{\"status\":\"' + latestStatusFromRobotDrive + '\"}');
+}
 
-const mainHandler = (request, response) => {
+const mainGetHandler = (request, response) => {
 
-    console.log('client request: mainHandler: ' + request.path);
+    console.log('client request: mainGetHandler: ' + request.path);
 
-
-    if (request.path === '/messaging/api/status') {
-        respondWithCollectedDataHandler(request, response);
+    if (request.path === '/messaging/api/command') {
+        response.status(200).send('{\"command\":\"' + latestCommandToRobotDrive + '\"}');
         return;
     }
 
-    response.status(500).send('{\"error\":\"Error: ' + e + ' : You requested ' + request.path + '. Unknown.\"}');
+    if (request.path === '/messaging/api/status') {
+        response.status(200).send('{\"status\":\"' + latestStatusFromRobotDrive + '\"}');
+        return;
+    }
+
+
+
+    console.log(request.method);
+    console.log(request.params);
+    response.status(500).send('{\"error\":\"Error: You requested ' + request.path + '. Unknown.\"}');
 }
-app.get('/messaging/api/*', mainHandler);
+app.get('/messaging/api/*', mainGetHandler);
+
+const mainPostHandler = (request, response) => {
+
+    console.log('client request: mainPostHandler: ' + request.path);
+
+
+    if (request.path === '/messaging/api/command') {
+        console.log('query.uri:' + request.query.uri);
+        console.log('query.params:' + JSON.stringify(request.params));
+        latestCommandToRobotDrive = request.query.uri;
+        response.status(201).send('{\"msg\":\"ok\"}');
+        return;
+    }
+
+
+    console.log(request.method);
+    console.log(request.params);
+    response.status(500).send('{\"error\":\"Error: You requested ' + request.path + '. Unknown.\"}');
+}
+app.post('/messaging/api/*', mainPostHandler);
 
 
 
 const messagingApiBad = (request, response) => {
     console.log('client request: messagingApiBad : ' + request.path);
+    console.log(request.method);
+    console.log(request.params);
     response.status(404).send('{\"error\":\"You requested ' + request.path + '. You need /api/blah.blah after that.\"}');
 }
 app.get('/messaging/api', messagingApiBad);
@@ -43,6 +78,8 @@ app.post('/messaging/api', messagingApiBad);
 
 const messagingBad = (request, response) => {
     console.log('client request: messagingBad : ' + request.path);
+    console.log(request.method);
+    console.log(request.params);
     response.status(404).send('{\"error\":\"You requested ' + request.path + '. You need /api/blah blah after that.\"}');
 }
 app.get('/messaging', messagingBad);
@@ -51,13 +88,15 @@ app.post('/messaging', messagingBad);
 
 const badRoot = (request, response) => {
     console.log('client request: badRoot : ' + request.path);
+    console.log(request.method);
+    console.log(request.params);
     response.status(404).send('{\"error\":\"You requested ' + request.path + '. You need /messaging/api/blah blah after that.\"}');
 }
-app.get('/', rootHandler);
-app.post('/', rootHandler);
+app.get('/', badRoot);
+app.post('/', badRoot);
 
 
-app.listen(thiServerPort, () => {
+app.listen(thisServerPort, () => {
     console.log('HTTP Raspberry Pi Server is Up at ', thisServerPort);
 });
 
---
+++ Python/OpenCV/FollowMe/loopFollowMeBasedOnMessaging.py
@@ -0,0 +1,393 @@
+import sys
+import argparse
+import os
+import pyttsx3
+import numpy as np
+import cv2
+import time
+from signal import signal, SIGINT
+import requests
+import traceback
+import socket
+import urllib3
+import json
+
+parser = argparse.ArgumentParser(prog=sys.argv[0], description='detect object with webcam & opencv', allow_abbrev=False)
+parser.add_argument('--width',type=int, dest='width', required=True)
+parser.add_argument('--height',type=int, dest='height', required=True)
+parser.add_argument('--FPS',type=int, dest='FPS', required=True)
+parser.add_argument('--http-timeout',type=float, dest='httpTimeout', required=True)
+parser.add_argument('--lSpeed',type=int, dest='leftSpeed', required=True)
+parser.add_argument('--rSpeed',type=int, dest='rightSpeed', required=True)
+parser.add_argument('--fbSpeed',type=int, dest='fwdBakSpeed', required=True)
+parser.add_argument('--loopDelay',type=float, dest='loopDelay', required=True)
+parser.add_argument('--min-dist',type=int, dest='minDist', required=True)
+parser.add_argument('--max-dist',type=int, dest='maxDist', required=True)
+parser.add_argument('--limit-buffer',dest='limitBuffer', action='store_true')
+parser.add_argument('--speak',dest='speak', action='store_true')
+args = parser.parse_args()
+
+width=args.width
+height=args.height
+FPS=args.FPS
+httpTimeout=args.httpTimeout
+leftSpeed=args.leftSpeed
+rightSpeed=args.rightSpeed
+fwdBakSpeed=args.fwdBakSpeed
+loopDelay=args.loopDelay
+minDist=args.minDist
+maxDist=args.maxDist
+limitBuffer=args.limitBuffer
+speak=args.speak
+
+robotDriveUrl = 'http://10.0.0.58:8084'
+robotMessagingUrl = 'http://10.0.0.58:8085/messaging/api'
+robotIsReadyToDrive = False
+robotDriveServerConnectionRefusedNumberOfTimes = 0;
+messagingServerConnectionRefusedNumberOfTimes = 0;
+
+cap = None
+
+xml_path = '/home/devchu/.virtualenvs/cv/lib/python3.7/site-packages/cv2/data/'
+face_cascade = cv2.CascadeClassifier(xml_path + 'haarcascade_frontalface_default.xml')
+
+
+##################################################################
+# these program flow control, mainly just to make sure we do not
+# repeat(say) something uneccesarily.
+noFaceDetected = False
+faceDetected   = False
+faceCentered   = False
+tooManyFaces   = False
+faceMovedLeft  = False
+faceMovedRight = False
+faceTooClose   = False
+faceTooFar     = False
+faceIsJustRight= False
+
+lastTimeMoved = time.time()
+
+tts = pyttsx3.init()
+
+##################################################################
+def initCamera():
+    global cap
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    cap.set(cv2.CAP_PROP_FPS, FPS)
+
+    if limitBuffer:
+        cap.set(cv2.CAP_PROP_BUFFERSIZE,1)
+
+
+##################################################################
+def cleanUpCamera():
+    cap.release()
+    cv2.destroyAllWindows()
+
+##################################################################
+def cleanUp():
+    cleanUpCamera()
+    print('Done. Bye,bye.')
+    sys.exit(0)
+
+
+##################################################################
+def signalHandler(signalReceived, frame):
+    print('Got CTRL-C...')
+    cleanUp()
+
+
+##################################################################
+def say(phrase):
+    if speak:
+        tts.say(phrase)
+        tts.runAndWait()
+    print(phrase)
+
+##################################################################
+def sendGetMessage(completeUriString):
+
+    global messagingServerConnectionRefusedNumberOfTimes
+
+    try:
+        print(robotMessagingUrl + completeUriString)
+        response = requests.get(url=robotMessagingUrl + completeUriString, timeout=httpTimeout)
+        messagingServerConnectionRefusedNumberOfTimes = 0;
+        return response.text
+    except (socket.timeout, urllib3.exceptions.ReadTimeoutError, requests.exceptions.ReadTimeout):
+        return 'Timeout'
+    except (requests.exceptions.ConnectionError, ConnectionRefusedError):
+        time.sleep(0.20)
+        messagingServerConnectionRefusedNumberOfTimes += 1;
+        if messagingServerConnectionRefusedNumberOfTimes > 3:
+            cleanUp()
+        return 'Refused'
+    except:
+        track = traceback.format_exc()
+        print(track)
+        say('Other Communication Error', 0)
+        cleanUp()
+
+
+##################################################################
+def sendPostMessage(completeUriString):
+
+    global messagingServerConnectionRefusedNumberOfTimes
+
+    try:
+        print(robotMessagingUrl + completeUriString)
+        response = requests.post(url=robotMessagingUrl + completeUriString, timeout=httpTimeout)
+
+        messagingServerConnectionRefusedNumberOfTimes = 0;
+        return response.text
+    except (socket.timeout, urllib3.exceptions.ReadTimeoutError, requests.exceptions.ReadTimeout):
+        return 'Timeout'
+    except (requests.exceptions.ConnectionError, ConnectionRefusedError):
+        time.sleep(0.20)
+        messagingServerConnectionRefusedNumberOfTimes += 1;
+        if messagingServerConnectionRefusedNumberOfTimes > 3:
+            cleanUp()
+        return 'Refused'
+    except:
+        track = traceback.format_exc()
+        print(track)
+        say('Other Communication Error', 0)
+        cleanUp()
+
+##################################################################
+def executeCommandIfAnyFromMessaging():
+        possibleJsonResponse = sendGetMessage('/command')
+        response = json.loads(possibleJsonResponse)
+        #print(response)
+        if 'command' in response.keys():
+            response = sendRobotUrl(response['command'])
+            print('response back from robot command ' + response)
+
+
+##################################################################
+def sendRobotUrl(command):
+
+    global robotDriveServerConnectionRefusedNumberOfTimes
+
+    try:
+        response = requests.get(robotDriveUrl + command, timeout=httpTimeout)
+        robotDriveServerConnectionRefusedNumberOfTimes = 0;
+        return response.text
+    except (socket.timeout, urllib3.exceptions.ReadTimeoutError, requests.exceptions.ReadTimeout):
+        return 'Timeout'
+    except (requests.exceptions.ConnectionError, ConnectionRefusedError):
+        time.sleep(0.20)
+        robotDriveServerConnectionRefusedNumberOfTimes += 1;
+        if robotDriveServerConnectionRefusedNumberOfTimes > 3:
+            cleanUp()
+        return 'Refused'
+    except:
+        track = traceback.format_exc()
+        print(track)
+        say('Other Communication Error')
+        cleanUp()
+
+##################################################################
+def initRobotDrive():
+    respText = sendRobotUrl('/nodejs/api/data')
+    if '"volts":-1' in respText:
+        say('Arduino is Up, but Not Roboclaw')
+        cleanUp()
+    time.sleep(0.5)
+    respText = sendRobotUrl('/arduino/api/clr.usb.err')
+    if 'Cmd Sent To Arduino' in respText:
+        time.sleep(1)
+        respText = sendRobotUrl('/nodejs/api/data')
+        if 'NEEDINIT' in respText:
+            return False
+        say('Robot Is Ready.')
+        return True
+    else:
+        print(respText)
+        say(respText)
+    return False
+
+##################################################################
+def sendRobotDriveCommand(direction, mySpeed):
+    global robotIsReadyToDrive
+    if not robotIsReadyToDrive:
+        robotIsReadyToDrive = initRobotDrive()
+    if robotIsReadyToDrive:
+        respText = sendRobotUrl('/arduino/api/' + direction + '/' + str(mySpeed))
+        if not 'Cmd Sent To Arduino' in respText:
+            print(respText)
+            say(respText)
+
+##################################################################
+def sendRobotNodeJsCommand(command, resultExpected, sayOnGoodResult):
+    global robotIsReadyToDrive
+    if not robotIsReadyToDrive:
+        robotIsReadyToDrive = initRobotDrive()
+    if robotIsReadyToDrive:
+        respText = sendRobotUrl('/nodejs/api/' + command)
+        if not 'volts\":12' in respText:
+            print(respText)
+            say(respText)
+        else:
+            say(sayOnGoodResult)
+
+
+
+
+##################################################################
+def moveLeftOrRightToCenterOnFace(deltaLastTimeMoved, deltaLeftRight, leftEdge, rightEdge):
+    global lastTimeMoved
+    global faceMovedRight
+    global faceMovedLeft
+
+    if deltaLastTimeMoved > loopDelay and deltaLeftRight > 45:
+
+        if leftEdge > rightEdge:
+            if not faceMovedRight:
+                say('right')
+                faceMovedRight = True
+            sendRobotDriveCommand('right',rightSpeed)
+        else:
+            if not faceMovedLeft:
+                say('left')
+                faceMovedLeft = True
+            sendRobotDriveCommand('left',leftSpeed)
+
+        lastTimeMoved = time.time()
+
+
+##################################################################
+def moveMoveForwardOrBackForCorrectDistanceAway():
+    global lastTimeMoved
+    global faceCentered
+    global faceIsJustRight
+    global faceTooClose
+    global faceTooFar
+
+    if not faceCentered:
+        say('center')
+        faceCentered = True
+
+    if w > minDist and deltaLastTimeMoved > loopDelay:
+        faceIsJustRight = False
+        if not faceTooClose:
+            say('close')
+            faceTooClose = True
+        sendRobotDriveCommand('backward',fwdBakSpeed)
+        lastTimeMoved = time.time()
+    elif w < maxDist and deltaLastTimeMoved > loopDelay:
+        faceIsJustRight = False
+        if not faceTooFar:
+            say('far')
+            faceTooFar = True
+        sendRobotDriveCommand('forward',fwdBakSpeed)
+        lastTimeMoved = time.time()
+    elif w >= maxDist and w<=minDist:
+        faceTooClose = False
+        faceTooFar = False
+        if not faceIsJustRight:
+            faceIsJustRight = True
+            say('Stay there')
+
+
+##################################################################
+##################################################################
+# main program
+##################################################################
+##################################################################
+
+if __name__ == '__main__':
+    signal(SIGINT, signalHandler)
+
+initCamera()
+
+robotIsReadyToDrive = initRobotDrive()
+if not robotIsReadyToDrive:
+    robotIsReadyToDrive = initRobotDrive()
+    if not robotIsReadyToDrive:
+        robotIsReadyToDrive = initRobotDrive()
+
+if not robotIsReadyToDrive:
+    say('Robot not initialized')
+    cleanUp()
+
+
+# only attempt to read if it is opened
+if cap.isOpened:
+    while(True):
+        ret, frame = cap.read()
+
+        executeCommandIfAnyFromMessaging()
+        
+        gray = None
+        try:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        except:
+            cleanUpCamera()
+            time.sleep(0.5)
+            initCamera()
+
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+        if len(faces) < 1:
+            if not noFaceDetected:
+                say('No one')
+                noFaceDetected = True
+            faceDetected = False
+            tooManyFaces = False
+            faceCentered = False
+            faceMovedLeft = False
+            faceMovedRight = False
+            faceTooClose   = False
+            faceTooFar     = False
+            faceIsJustRight = False
+            continue
+
+        elif len(faces) > 2:
+            if not tooManyFaces:
+                say('too many faces')
+                tooManyFaces = True
+            noFaceDetected = False
+            faceDetected = False
+            continue
+
+        elif len(faces) > 0:
+            noFaceDetected = False
+            if not faceDetected:
+                say('I see you')
+                faceDetected = True
+                #continue
+
+        tooManyFaces = False
+        faceIdx = 0
+        for (x, y, w, h) in faces:
+
+            noFaceDetected = False
+
+            leftEdge = x
+            rightEdge = width - (x + w)
+            print(leftEdge,' ',rightEdge)
+            deltaLeftRight = abs(leftEdge - rightEdge)
+
+            deltaLastTimeMoved = time.time() - lastTimeMoved
+
+            if deltaLeftRight <= 45:
+
+                faceMovedLeft = False
+                faceMovedRight = False
+
+            else:
+                faceCentered = False
+                faceIsJustRight = False
+
+            moveMoveForwardOrBackForCorrectDistanceAway()
+
+ 
+            moveLeftOrRightToCenterOnFace(deltaLastTimeMoved, deltaLeftRight, leftEdge, rightEdge)
+ 
+else:
+    print("Failed to open capture device")
+
